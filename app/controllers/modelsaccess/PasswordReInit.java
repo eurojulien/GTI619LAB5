806368994dddd5550f52b9134d13046de7fb62b2
Julien Drolet-Noel <eurojulien@gmail.com>
2014-11-25 15:19:09 -0500

    PasswordReInit -> Controlleur pour la classe Passwords

---
+++ app/controllers/modelsaccess/PasswordReInit.java
@@ -0,0 +1,56 @@
+package controllers.modelsaccess;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.h2.util.Task;
+
+import com.avaje.ebean.Ebean;
+
+import models.Roles;
+import models.Users;
+import models.Passwords;
+
+
+public class PasswordReInit {
+	
+	private static PasswordReInit instance = null;
+	
+	private PasswordReInit(){};
+	
+	public static PasswordReInit Password(){
+		if (instance == null){
+			instance = new PasswordReInit();
+		}
+		
+		return instance;
+	}
+	
+	public boolean addPasswordReInitialisation(
+			String login,
+			String email,
+			int availableTimeInSeconds,
+			String passphrase)
+	{
+		
+		Users user = Ebean.find(Users.class)
+		.where()
+		.eq("login", login)
+		.eq("email", email)
+		.findUnique();
+		
+		if (user == null){
+			return false;
+		}
+		
+		Passwords password = new Passwords();
+		
+		password.user = user;
+		
+		
+		return true;
+	}
+	
+	
+}

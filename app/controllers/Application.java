ab1f44a40b2e2f67f06451a8b820175929a002fc
ayuuki <missabenoit@gmail.com>
2014-11-25 16:04:50 -0500

    Application.java ajout de login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,9 @@
 package controllers;
 
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -10,5 +11,35 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    public static Result login() {
+        return ok(
+            login.render(Form.form(Login.class))
+        );
+    }
+    public static Result authenticate() {
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+    public static class Login {
 
+        public String email;
+        public String password;
+        
+        public String validate() {
+          //  if (User.authenticate(email, password) == null) {
+         //     return "Invalid user or password";
+           // }
+            return null;
+        }
+
+    }
 }
